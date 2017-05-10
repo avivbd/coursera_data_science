2212849169e827fbc957a9a54d491d06f10885a6
Aviv Bachan <avivbd@yahoo.com>
2017-05-09 21:39:06 -0700

    added simple gradient descent code written in the programming warmup

---
+++ Assignments/assignment_1/simple_gradient_descent.m
@@ -0,0 +1,39 @@
+function [] = simple_gradient_descent()
+%     rng(1985)
+    A = [ones(10,1), randn(10,3)];
+    b = rand(10,1);
+
+    x_init= zeros(4,1);
+    alpha = 0.01;
+    [x, n_iter, rtols] = matrixInverseVector(A, b, x_init, alpha);
+
+    disp('n iterations')
+    disp(n_iter)
+    disp('x = ')
+    disp(x)
+    disp('A\b = ')
+    disp(A\b)
+    
+    plot(log10(rtols))
+    
+end
+
+
+function [x, n_iter, rtols] = matrixInverseVector(A, b, x_init, alpha)
+    x = x_init;
+    n_iter = 1;
+    rtols = [];
+    while true
+        grad = 2*A'*(A*x-b);
+        xn = x - alpha*grad;
+        sse = @(x) norm(A*x-b);
+        rtol = abs(sse(xn) - sse(x))/sse(x);
+        if rtol<1e-12
+            break
+        else
+            x = xn;
+            n_iter = n_iter + 1;
+            rtols(n_iter) = rtol;
+        end
+    end  
+end
